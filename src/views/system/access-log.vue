c7f1269ac98123bc6151ff80423028c39918c259
sunhz <mymailcache@163.com>
2022-03-17 10:03:39 +0800

    调整日志弹窗样式

--- src/views/system/access-log.vue
+++ src/views/system/access-log.vue
@@ -90,7 +90,16 @@
         :close-on-click-modal="false"
         :visible.sync="logVisible"
       >
-        <el-descriptions title="访问日志信息">
+        <el-descriptions title="接口信息">
+          <el-descriptions-item label="方法名称">{{ log.method }}</el-descriptions-item>
+          <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
+          <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
+          <el-descriptions-item label="备注">
+            <el-tag size="small">学校</el-tag>
+          </el-descriptions-item>
+          <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+        </el-descriptions>
+        <el-descriptions title="调用信息">
           <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
           <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
           <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
