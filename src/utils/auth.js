08e7bd36d1d85ae68080f8dbec13f496183e91db
sunhz <mymailcache@163.com>
2022-12-19 15:59:41 +0800

    修改TokenKey，预防重复token问题

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,7 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'Admin-Token'
+// todo 不同项目注意修改自己的键值，预防同框架项目互相干扰
+const TokenKey = 'BF_TOKEN'
 
 export function getToken() {
   return Cookies.get(TokenKey)
